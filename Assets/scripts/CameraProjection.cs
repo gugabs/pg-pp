9c18edf3e3f6c7f1ae90e69dfde69bbc893202a9
Guga <contato.gustavobarbosa@outlook.com>
2021-11-18 22:36:26 -0300

    segunda câmera, alteração de projeção, animação da hélice principal e avião

---
+++ Assets/scripts/CameraProjection.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraProjection : MonoBehaviour
+{
+    public Camera mainCamera;
+
+    void Update()
+    {
+        if (Input.GetKeyUp("p")) {
+            mainCamera.orthographic = !mainCamera.orthographic;
+        }
+    }
+}
---
+++ Assets/scripts/CameraSwitch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    public Camera mainCamera;
+    public Camera pilotCamera;
+
+    void Start()
+    {
+        mainCamera.enabled = true;
+        pilotCamera.enabled = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp("c")) {
+            mainCamera.enabled = !mainCamera.enabled;
+            pilotCamera.enabled = !pilotCamera.enabled;
+        } 
+    }
+}
